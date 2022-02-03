330ae8ecd2b1acd7e9a531036554865e3b08cfa3
yjh4507 <yyjh4507@naver.com>
2022-02-03 17:40:18 +0900

    2022-02-03

--- js/sub_ajax.js
+++ js/sub_ajax.js
@@ -10,6 +10,7 @@ $.ajax({
         $(".sec1_box_1>.box1").children(".box1_text").append("<p>" + msg.documents[0].authors + "</p>");
         $(".sec1_box_1>.box1").children(".box1_text").append("<p>" + msg.documents[0].publisher + "</p>");
     });
+// -----------책정보------------
 
     $(function(){
         $.get("./sub_txt/txt1.txt", function (data){
@@ -17,3 +18,33 @@ $.ajax({
         })
     });
 
+    $(function(){
+        $.get("./sub_txt/txt2.txt", function (data){
+            $(".info_box2").html(data);
+        })
+    });
+
+
+    $(function(){
+        $.get("./sub_txt/txt3.txt", function (data){
+            $(".info_box3").html(data);
+        })
+    });
+
+// ---------아코디언-------------
+// $("h4.h4title1").on('click',function(){
+//     $(this).next(".con").slideToggle(100);
+// });
+
+// $('h4.h4title1').on('click', function () {
+
+//     if ($(this).hasClass('on')) {
+//         slideUp();
+//     } else {
+//         slideUp();
+//         $(this).addClass('on').next().slideDown();
+//     }
+//     function slideUp() {
+//         $('h4.h4title1').removeClass('on').next().slideUp();
+//     };
+// })
