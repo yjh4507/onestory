bdbd424ea42ca06d2335f499e23aadc967d49aea
yjh4507 <93234827+yjh4507@users.noreply.github.com>
2022-02-04 12:40:45 +0900

    2022-02-04

--- js/sub_ajax.js
+++ js/sub_ajax.js
@@ -10,7 +10,7 @@ $.ajax({
         $(".sec1_box_1>.box1").children(".box1_text").append("<p>" + msg.documents[0].authors + "</p>");
         $(".sec1_box_1>.box1").children(".box1_text").append("<p>" + msg.documents[0].publisher + "</p>");
     });
-// -----------책정보------------
+// -----------메모장 책정보------------
 
     $(function(){
         $.get("./sub_txt/txt1.txt", function (data){
@@ -24,27 +24,105 @@ $.ajax({
         })
     });
 
-
     $(function(){
         $.get("./sub_txt/txt3.txt", function (data){
             $(".info_box3").html(data);
         })
     });
 
-// ---------아코디언-------------
-// $("h4.h4title1").on('click',function(){
-//     $(this).next(".con").slideToggle(100);
-// });
+// --------------------로그인후 이용가능----------------------
+$(document).ready(function () {
+    $('.sec1_purchase>p').click(function () {
+      alert("로그인이 필요한 서비스입니다.")
+    });
+    $('.comment_a').click(function () {
+        alert("로그인후 작성 가능합니다.")
+      });
+});
+// --------------------------추천----------------------
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "김호연" },
+    headers: { Authorization: "KakaoAK 04506107d704df28638f24857e3ad74b" }
+  })
+      .done(function (msg) {
+        var boxs = document.getElementsByClassName('box');
+        for (var i = 0; i < boxs.length; i++) {
+          $(".sec3_box_3_1>.box").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+          $(".sec3_box_3_1>.box").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+          $(".sec3_box_3_1>.box").eq(i).append("<p>" + msg.documents[i].authors + "</p>");
+        }
+    });
 
-// $('h4.h4title1').on('click', function () {
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "소설" },
+    headers: { Authorization: "KakaoAK 04506107d704df28638f24857e3ad74b" }
+  })
+      .done(function (msg) {
+        var boxs = document.getElementsByClassName('box');
+        for (var i = 0; i < boxs.length; i++) {
+          $(".sec3_box_3_2>.box").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+          $(".sec3_box_3_2>.box").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+          $(".sec3_box_3_2>.box").eq(i).append("<p>" + msg.documents[i].authors + "</p>");
+        }
+    });
 
-//     if ($(this).hasClass('on')) {
-//         slideUp();
-//     } else {
-//         slideUp();
-//         $(this).addClass('on').next().slideDown();
-//     }
-//     function slideUp() {
-//         $('h4.h4title1').removeClass('on').next().slideUp();
-//     };
-// })
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "우리" },
+    headers: { Authorization: "KakaoAK 04506107d704df28638f24857e3ad74b" }
+  })
+      .done(function (msg) {
+        var boxs = document.getElementsByClassName('box');
+        for (var i = 0; i < boxs.length; i++) {
+          $(".sec3_box_3_3>.box").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+          $(".sec3_box_3_3>.box").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+          $(".sec3_box_3_3>.box").eq(i).append("<p>" + msg.documents[i].authors + "</p>");
+        }
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "내 편" },
+    headers: { Authorization: "KakaoAK 04506107d704df28638f24857e3ad74b" }
+  })
+      .done(function (msg) {
+        var boxs = document.getElementsByClassName('box');
+        for (var i = 0; i < boxs.length; i++) {
+          $(".sec3_box_3_4>.box").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+          $(".sec3_box_3_4>.box").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+          $(".sec3_box_3_4>.box").eq(i).append("<p>" + msg.documents[i].authors + "</p>");
+        }
+    });
+
+// -----------메뉴이동--------------
+$(function () {
+    $(".sec2_box_1").fadeIn();
+    $('.sec2_nav_ul>li:nth-of-type(1)').click(function () {
+      $('li').removeClass("active");
+      $(this).addClass("active");
+      $(".sec2_box_1").show();
+      $(".sec2_box_2").hide();
+      $(".sec2_box_3").hide();
+    });
+    $('.sec2_nav_ul>li:nth-of-type(2)').click(function () {
+      $('li').removeClass("active");
+      $(this).addClass("active");
+      $(".sec2_box_1").hide();
+      $(".sec2_box_2").show();
+      $(".sec2_box_3").hide();
+    });
+    $('.sec2_nav_ul>li:nth-of-type(3)').click(function () {
+      $('li').removeClass("active");
+      $(this).addClass("active");
+      $(".sec2_box_1").hide();
+      $(".sec2_box_2").hide();
+      $(".sec2_box_3").show();
+    });
+  });
+  
